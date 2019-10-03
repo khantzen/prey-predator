4e523116ef414808654d5c99d463ff391f3aacb7
k.hantzen <kevin.hantzen@gmail.com>
2019-10-03 21:50:38 +0200

    implement world generation

---
+++ src/domain/World.py
@@ -0,0 +1,16 @@
+from src.domain.Territory import Territory
+from src.domain.Rabbit import Rabbit
+from src.domain.Fox import Fox
+
+class World:
+    def __init__(self, line, column, rabbits, foxes, coord_generator):
+        self.grid = [ [Territory() for x in range(column) ] for x in
+                     range(line) ]
+
+        for rabbit in range(rabbits):
+            coord = coord_generator.next_coord()
+            self.grid[coord[0]][coord[1]].add_rabbit(Rabbit())
+
+        for fox in range(foxes):
+            coord = coord_generator.next_coord()
+            self.grid[coord[0]][coord[1]].add_fox(Fox())
---
+++ test/domain/world_test.py
@@ -0,0 +1,38 @@
+import unittest
+from src.domain.World import World
+from src.domain.Territory import Territory
+
+class WorldTest(unittest.TestCase):
+
+    def test_one_world_should_contain_a_dimensionned_grid_of_territory(self):
+        line = 10
+        column = 10
+        world = World(line, column, 0, 0,  None)
+        assert len(world.grid) == 10
+        assert len(world.grid[0]) == 10
+        assert type(world.grid[0][0]) is Territory
+
+    def test_one_rabbit_start_at_a_random_position(self):
+        rabbit_count = 1
+        world = World(10, 10, rabbit_count, 0, MockCoordGenerator())
+        assert world.grid[0][0].rabbit_count() == 1
+        assert world.grid[4][4].rabbit_count() == 0
+
+    def test_one_fox_start_at_a_random_position(self):
+        fox_count = 1
+        world = World(10, 10, 0, fox_count, MockCoordGenerator())
+        assert world.grid[0][0].fox_count() == 1
+        assert world.grid[3][6].fox_count() == 0
+
+class MockCoordGenerator():
+    def __init__(self):
+        self.coord = [[0,0], [0,0], [2,2]]
+        self.index = 0
+
+    def next_coord(self):
+        coord = self.coord[self.index]
+        self.index += 1
+        return coord
+
+if __name__ == '__main__':
+    unittest.main()
