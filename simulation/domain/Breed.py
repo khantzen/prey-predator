249089dac2b7d7988282160b6133411bc272e948
k.hantzen <kevin.hantzen@gmail.com>
2019-10-20 13:55:05 +0200

    create branch for java

--- simulation/domain/Breed.py
+++
@@ -1,6 +0,0 @@
-class Breed:
-    def __init__(self, unit_count):
-        self.unit_count = unit_count
-
-    def new_children(self):
-        return int(self.unit_count / 2)
--- simulation/domain/Hunt.py
+++
@@ -1,9 +0,0 @@
-class Hunt:
-    def __init__(self, foxes, rabbits):
-        self.foxes = foxes
-        self.rabbits = rabbits
-
-    def launch(self):
-        for fox, rabbit in zip(self.foxes, self.rabbits):
-            rabbit.kill()
-            fox.feed()
